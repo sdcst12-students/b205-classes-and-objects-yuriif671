77f6930242b25229cd56cab20a002c2c01cec37b
Chris Yang <cyang@deltalearns.ca>
2023-09-27 21:35:44 -0700

    initial

---
+++ task2.py
@@ -0,0 +1,22 @@
+#!python3
+"""
+Compound Interest Calculator
+Create a class object that accepts paramters for Principal, Annual Interest Rate, Number of compounding periods.  
+Create a class method that calculates the amount of compound interest for a given length of time.
+
+Extension: accept time given in different measurements, but convert them to years for use in your class template.
+"""
+
+
+class Calc:
+
+    def __init__(self):
+        #more input parameters needed
+        return
+
+    def interest(self,t):
+        return 
+
+a = Calc(P=1000,r=4,n=2)
+assert a.interest(3) == 2252.32
+
